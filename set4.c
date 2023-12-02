94926a73a136b2c32d9185778f00eb00eb3a4d41
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-02 19:43:50 +0100

    set4.c

---
+++ set4.c
@@ -0,0 +1,107 @@
+#include "shell.h"
+
+/**
+ * printEnvironment - Prints the current environment.
+ * @inform: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0.
+ */
+int printEnvironment(inform_t *inform)
+{
+    list_t *node = inform->env;
+
+    while (node != NULL)
+    {
+        printf("%s\n", node->str);
+        node = node->next;
+    }
+
+    return 0;
+}
+
+/**
+ * getenvValue - Gets the value of an environment variable.
+ * @inform: Structure containing potential arguments. Used to maintain
+ * @name: The name of the environment variable.
+ *
+ * Return: The value of the environment variable, or NULL if not found.
+ */
+char *getenvValue(inform_t *inform, const char *name)
+{
+    list_t *node = inform->env;
+    char *value = NULL;
+
+    while (node != NULL)
+    {
+        if (starts_with(node->str, name))
+        {
+            value = strchr(node->str, '=');
+            if (value != NULL)
+            {
+                value++;
+                return value;
+            }
+        }
+        node = node->next;
+    }
+
+    return NULL;
+}
+
+/**
+ * setEnvironment - Initializes a new environment variable or modifies an existing one.
+ * @inform: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * Return: 1 on success, 0 on failure.
+ */
+int setEnvironment(inform_t *inform)
+{
+    if (inform->argc != 3)
+    {
+        _eputs("Incorrect number of arguments\n");
+        return 1;
+    }
+    if (_setenv(inform, inform->argv[1], inform->argv[2]))
+        return 0;
+    return 1;
+}
+/**
+ * removeEnvironmentVariable - Removes an environment variable
+ * @inform: Structure containing relevant information. Used to maintain
+ *        consistent function prototype.
+ * Return: Always 0
+ */
+int removeEnvironmentVariable(inform_t *inform)
+{
+    int i;
+
+    if (inform->argc <= 1)
+    {
+        _eputs("Too few arguments.\n");
+        return (1);
+    }
+    for (i = 1; i < inform->argc; i++)
+        _unsetenv(inform, inform->argv[i]);
+
+    return (0);
+}
+
+/**
+ * populateEnvironmentList - Populates the environment linked list
+ * @inform: Structure containing relevant information. Used to maintain
+ *          consistent function prototype.
+ * Return: Always 0
+ */
+int populateEnvironmentList(inform_t *inform)
+{
+    list_t *node = NULL;
+    size_t i = 0;
+
+    while (environ[i])
+    {
+        addNodeEnd(&node, environ[i], 0);
+        i++;
+    }
+    inform->env = node;
+    return (0);
+}
