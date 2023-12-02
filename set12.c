8cb19b5497ce74c481e000aba276dcd84136ace3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-02 22:32:18 +0100

    set12.c

--- set12.c
+++ set12.c
@@ -1,14 +1,14 @@
 #include "shell.h"
 
 /**
- * add__new_node - adds a node to the start of the list
+ * add_newnode - adds a node to the start of the list
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
  *
  * Return: size of list
  */
-list_t *add__new_node(list_t **head, const char *str, int num)
+list_t *add_newnode(list_t **head, const char *str, int num)
 {
     list_t *new_head;
 
