c28ad0945b3b19b6c5d4c65c5c4fc6da4a39a4c6
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-11 23:56:56 +0100

    Update set6.c

--- set6.c
+++ set6.c
@@ -30,6 +30,7 @@ void print_error_message(inform_t *inform, char *error_str)
 int print_decimal_number(int n, int file_des) {
 int count = 0;
 unsigned int absolute, current;
+int (*__putchar)(char) = _putchar;
 if (file_des == STDERR_FILENO)
     _putchar = _eputchar;
 
