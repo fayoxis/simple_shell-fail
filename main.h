9242dd16b764f5cd0f4d7911cd6bbbfeea3e5375
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-11 22:30:18 +0100

    Update main.h

--- main.h
+++ main.h
@@ -49,51 +49,50 @@ typedef struct liststr
 	char *str;
 	struct liststr *next;
 } list_t;
-
 /**
- *struct passinfo - contains pseudo-arguements to pass into a function,
- *		allowing uniform prototype for function pointer struct
- *@arg: a string generated from getline containing arguements
- *@argv: an array of strings generated from arg
- *@path: a string path for the current command
- *@argc: the argument count
- *@line_count: the error count
- *@err_num: the error code for exit()s
- *@linecount_flag: if on count this line of input
- *@fname: the program filename
- *@env: linked list local copy of environ
- *@environ: custom modified copy of environ from LL env
- *@history: the history node
- *@alias: the alias node
- *@env_changed: on if environ was changed
- *@status: the return status of the last exec'd command
- *@cmd_buf: address of pointer to cmd_buf, on if chaining
- *@cmd_buf_type: CMD_type ||, &&, ;
- *@readfd: the fd from which to read line input
- *@histcount: the history line number count
+ * struct passinginform - Contains pseudo-arguments to pass into a function,
+ *                  allowing a uniform prototype for a function pointer struct.
+ * @argument: A string generated from `getline` containing arguments.
+ * @arguments: An array of strings generated from `argument`.
+ * @path: A string path for the current command.
+ * @argumentCount: The count of arguments.
+ * @line_count: The line count.
+ * @error_num: The error code for `exit()`.
+ * @linecount_flag: If on, count this line of input.
+ * @file_name: The program filename.
+ * @env: Linked list local copy of `environ`.
+ * @history: The history node.
+ * @alias: The alias node.
+ * @environment: Custom modified copy of `environ` from linked list `env`.
+ * @env_changed: On if `environ` was changed.
+ * @status: The return status of the last executed command.
+ * @commandBuffer: Address of pointer to command buffer, on if chaining.
+ * @cmd_buffer_type: Command type (`CMD_type`), `||`, `&&`, or `;`.
+ * @readfd: The file descriptor from which to read line input.
+ * @historyCount: The history line number count.
  */
-typedef struct passinform
+typedef struct passinginform
 {
-	char *argument;
-	char **arguments;
-	char *path;
-	int argumentCount ;
-	unsigned int line_count;
-	int error_num;
-	int linecount_flag;
-	char *file_name;
-	list_t *env;
-	list_t *history;
-	list_t *alias;
-	char **environment;
-	int env_changed;
-	int status;
+    char *argument;
+    char **arguments;
+    char *path;
+    int argumentCount;
+    unsigned int line_count;
+    int error_num;
+    int linecount_flag;
+    char *file_name;
+    list_t *env;
+    list_t *history;
+    list_t *alias;
+    char **environment;
+    int env_changed;
+    int status;
 
-	char **commandBuffer; /* pointer to cmd ; chain buffer, for memory mangement */
-	int cmd_buffer_type; /* CMD_type ||, &&, ; */
-	int readfd;
-	int historyCount;
-} info_t;
+    char **commandBuffer; /* Pointer to command ';' chain buffer, for memory management */
+    int cmd_buffer_type; /* Command type (`CMD_type`), `||`, `&&`, or `;` */
+    int readfd;
+    int historyCount;
+} inform_t;
 
 #define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
@@ -114,3 +113,66 @@ typedef struct builtin
 int checkInteractiveMode(info_t *infoPtr);
 int checkDelimiter(char ch, char *delimStr);
 int isAlphabetic(int ch);
+int convertStringToInteger(char *str);
+int exit_shell(inform_t *inform);
+int change_Directory(inform_t *inform);
+int custom_help(inform_t *inform);
+int display_history(inform_t *inform);
+int remove_alias(inform_t *inform, char *alias);
+int setAlias(inform_t *inform, char *str);
+
+int printAlias(list_t *node);
+int myAlias(inform_t *inform);
+int printEnvironment(inform_t *inform);
+char *_getenv(inform_t *inform, const char *name);
+int setEnvironment(inform_t *inform);
+int removeEnvironmentVariable(inform_t *inform);
+int populateEnvironmentList(inform_t *inform);
+void _eputs(char *str);
+int _eputchar(char ch);
+int _putfd(char ch, int file_des);
+int _putsfd(char *str, int file_des);
+int str_integer(char *str);
+void print_error_message(inform_t *inform, char *error_str);
+int print_decimal_number(int n, int file_des);
+char *convert_number_to_string(long int n, int b, int flags);
+void remove_comments_from_string(char *buffer);
+char *_strncpy(char *str, const char *src, int n);
+char *_strncat(char *str, const char *src, int n);
+char *_strchr(const char *str, char ch);
+ssize_t buffer_input(inform_t *inform, char **buffer, size_t *length);
+ssize_t get_input(inform_t *inform);
+ssize_t read_Buffer(info_t *info, char *buffer, size_t *size);
+
+int get_the_Line(info_t *info, char **ptr, size_t *length);
+void signal_Handler(__attribute__((unused))int signalNum);
+char **get_environment(inform_t *inform);
+int _unsetenv(inform_t *inform, char *var);
+int _setenv(inform_t *inform, char *var, char *value);
+void initialize_shell_inform(inform_t *_inform);
+
+void set_shell_inform(inform_t *_inform, char **arguments);
+void free_shell_info(inform_t *_inform, int freeAll);
+char *gethistory_file(info_t *info);
+int write_the_history(inform_t *inform);
+int readHistoryFromFile(inform_t *inform);
+int buildHistoryList(inform_t *inform, char *buffer, int line_count);
+int renumberHistory(inform_t *inform);
+void freeList(list_t **headPtr);
+list_t *add_newnode(list_t **head, const char *str, int num);
+list_t *addnode_tothe_end(list_t **head, const char *str, int num);
+size_t print_liststr(const list_t *head);
+int delete_nodeindex(list_t **head, unsigned int index);
+size_t get_list_length(const list_t *head);
+char **convert_list_to_strings(list_t *head);
+size_t print_linked_list(const list_t *head);
+list_t *find_node_starts_with(list_t *head, char *prefix, char c);
+ssize_t get_nodeindex(list_t *head, list_t *node);
+int _strlen(char *str);
+int _strcmp(char *str1, char *str2);
+char *it_starts_with(const char *haystack, const char *needle);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strdup(const char *str);
+void _puts(char *str);
+int _putchar(char ch);
