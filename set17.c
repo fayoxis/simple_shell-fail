381d9bcb3657f11b70c617c5407e77452a00b40c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-12 09:38:56 +0100

    Create set17.c

---
+++ set17.c
@@ -0,0 +1,66 @@
+
+#include "shell.h"
+
+/**
+ * memset - fills memory with a constant byte
+ * @mem: pointer to the memory area
+ * @val: the byte value to fill *mem with
+ * @size: the number of bytes to be filled
+ * Return: a pointer to the memory area mem
+ */
+char *memset(char *mem, char val, unsigned int size)
+{
+    for (unsigned int i = 0; i < size; i++) {
+        *(mem + i) = val;
+    }
+    return mem;
+}
+
+/**
+ * free_s - frees an array of strings
+ * @str_arr: array of strings
+ */
+void free_s(char **str_arr)
+{
+    if (str_arr == NULL) {
+        return;
+    }
+    char **temp = str_arr;
+    while (*str_arr != NULL) {
+        free(*str_arr);
+        str_arr++;
+    }
+    free(temp);
+}
+
+/**
+ * realloc - reallocates a block of memory
+ * @ptr: pointer to previously allocated block
+ * @old_size: size of the previous block in bytes
+ * @new_size: size of the new block in bytes
+ *
+ * Return: pointer to the old block.
+ */
+void *realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+    if (ptr == NULL) {
+        return malloc(new_size);
+    }
+    if (new_size == 0) {
+        free(ptr);
+        return NULL;
+    }
+    if (new_size == old_size) {
+        return ptr;
+    }
+    char *new_ptr = malloc(new_size);
+    if (new_ptr == NULL) {
+        return NULL;
+    }
+    unsigned int min_size = old_size < new_size ? old_size : new_size;
+    for (unsigned int i = 0; i < min_size; i++) {
+        *(new_ptr + i) = *((char *)ptr + i);
+    }
+    free(ptr);
+    return new_ptr;
+}
