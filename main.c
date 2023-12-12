8dfb30d6b6eeaa66cf91566b9e92911732549dd7
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-12 16:40:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,53 +1,54 @@
 #include "shell.h"
 
-int main(void) {
-    inform_t inform; /** Initialize the structure with default values */
-    ssize_t line_size;
-    char *line_buf = NULL;
-    size_t line_buf_size = 0;
+int main(void)
+{
+    inform_t inform = INFO_INIT;
+    char *buffer = NULL;
+    size_t length = 0;
+    ssize_t input_size;
 
-    initialize_shell_inform(&inform); /** Initialize shell information e.g., populate environment variables */
+    // Initialize shell environment and read history
+    initialize_shell_inform(&inform);
+    readHistoryFromFile(&inform);
 
-    while (1) {
-        printf("$ "); /** Print the prompt */
-        /** Read input from user */
-#if USE_GETLINE
-        line_size = getline(&line_buf, &line_buf_size, stdin);
-#else
-        line_size = get_the_Line(&inform, &line_buf, &line_buf_size);
-#endif
-        if (line_size <= 0) { /** Check for no input or error */
-            if (feof(stdin)) { /** Do necessary clean-up if end-of-file is encountered */
-                free(line_buf);
-                write_the_history(&inform);
-                free_shell_info(&inform, 1);
-                exit(inform.status);
-            }
-            continue; /** Continue the loop to prompt for input again in other cases */
+    // Main shell loop
+    while (1)
+    {
+        // Display prompt and get user input
+        _puts("$ ");
+        input_size = get_input(&inform);
+
+        if (input_size == -1)
+            break; // Exit the shell on EOF
+
+        // Skip empty lines
+        if (input_size > 1)
+        {
+            // Process the command buffer
+            remove_comments_from_string(inform.commandBuffer);
+            if (replacealias(&inform))
+                continue;
+
+            if (replacevariables(&inform))
+                continue;
+
+            // Add the command to history
+            buildHistoryList(&inform, inform.commandBuffer, inform.line_count);
+
+            // Execute the command (you'll need to implement this)
+            // execute_command(&inform);
+
+            // Reset command buffer for the next iteration
+            free(inform.commandBuffer);
+            inform.commandBuffer = NULL;
         }
-
-        /** Process input */
-        inform.argument = line_buf; /** Set the line buffer as the argument to pass around */
-        inform.argumentCount = 0; /** Reset argument count since this is a new line */
-        if (inform.argument && *inform.argument) { /** If a command is present */
-            if (buildHistoryList(&inform, inform.argument, inform.historyCount) != 0) { /** Add command to history */
-                /** Handle command execution like tokenizing the input and executing commands */
-                /** Handle_execution(&inform); */
-            }
-        }
-
-        /** Reset line buffer for the next command */
-        line_buf = NULL;
-        line_buf_size = 0;
     }
 
-    /** Perform clean-up on CTRL+D / exit command */
-    if (line_buf) {
-        free(line_buf);
-    }
+    // Write history to file before exiting
+    write_the_history(&inform);
 
-    write_the_history(&inform); /** Save history to the file */
-    free_shell_info(&inform, 1); /** Free allocated memory */
+    // Free allocated memory
+    free_shell_info(&inform, 1);
 
-    return inform.status; /** Return last status */
+    return 0;
 }
