d1566f88d0ebc0e4201c950a43936ce7f01222fe
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-12 11:28:48 +0100

    Update set17.c

--- set17.c
+++ set17.c
@@ -43,6 +43,10 @@ void free_s(char **str_arr)
  */
 void *my_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+    char *new_ptr;
+    unsigned int i;
+    unsigned int min_size;
+    
     if (ptr == NULL) {
         return malloc(new_size);
     }
@@ -53,12 +57,13 @@ void *my_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
     if (new_size == old_size) {
         return ptr;
     }
-    char *new_ptr = malloc(new_size);
+    new_ptr = malloc(new_size);
+    
     if (new_ptr == NULL) {
         return NULL;
     }
-    unsigned int min_size = old_size < new_size ? old_size : new_size;
-    for (unsigned int i = 0; i < min_size; i++) {
+    min_size = old_size < new_size ? old_size : new_size;
+    for (i = 0; i < min_size; i++) {
         *(new_ptr + i) = *((char *)ptr + i);
     }
     free(ptr);
