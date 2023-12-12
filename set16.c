96f4998f33daeb33cb73df9bcdcd731c194920f3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-12 09:12:27 +0100

    Create set16.c

---
+++ set16.c
@@ -0,0 +1,130 @@
+#include "shell.h"
+
+/**
+ * bfree - frees a pointer and sets it to NULL
+ * @ptr: address of the pointer to free
+ * @count: number of pointers to free
+ *
+ * Return: number of pointers successfully freed.
+ */
+int bfree(void **ptr, int count)
+{
+  int freed_count = 0;
+
+  for (int i = 0; i < count; i++)
+  {
+    if (ptr[i] != NULL)
+    {
+      free(ptr[i]); /* Free the memory pointed to by ptr[i] */
+      ptr[i] = NULL; /* Set ptr[i] to NULL */
+      freed_count++; /* Increment the counter*/
+    }
+  }
+
+  return freed_count; /* Return the number of pointers successfully freed */
+}
+/**
+ * isExecutableCmd - determines if a file is an executable command
+ * @inform: the inform struct
+ * @path: path to the file
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int isExecutableCmd(inform_t *inform, char *path)
+{
+    struct stat st;
+
+    (void)inform;
+    if (!path || stat(path, &st))
+        return 0;
+
+    if (st.st_mode & S_IFREG)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * duplicateChars - duplicates characters
+ * @pathstring: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *duplicateChars(const char *pathstring, int start, int stop)
+{
+    int length = stop - start;
+    char *buffer = (char *)malloc((length + 1) * sizeof(char));
+
+    if (buffer == NULL) {
+        return NULL;
+    }
+
+    int k = 0;
+    for (int i = start; i < stop; i++)
+    {
+        if (pathstring[i] != ':')
+        {
+            buffer[k] = pathstring[i];
+            k++;
+        }
+    }
+    buffer[k] = '\0';
+    return buffer;
+}
+/**
+ * findCmdPath - finds this command in the PATH string
+ * @inform: the inform struct
+ * @pathstring: the PATH string
+ * @command: the command to find
+ *
+ * Return: full path of command if found or NULL
+ */
+char *findCmdPath(inform_t *inform, char *pathstring, char *command)
+{
+    int current_pos = 0;
+    char *path = NULL;
+
+    if (!pathstring)
+        return NULL;
+
+    if (stringLength(command) > 2 && startsWith(command, "./"))
+    {
+        if (isExecutableCmd(inform, command))
+            return command;
+    }
+
+    for (int i = 0; pathstring[i]; i++)
+    {
+        int j = i;
+        while (pathstring[j] && pathstring[j] != ':')
+            j++;
+
+        path = duplicateChars(pathstring, current_pos, j);
+
+        if (!*path)
+            concatenate(path, command);
+        else
+        {
+            concatenate(path, "/");
+            concatenate(path, command);
+        }
+
+        if (isExecutableCmd(inform, path))
+            return path;
+
+        if (pathstring[j] == ':')
+        {
+            i = j + 1;
+            current_pos = i;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return NULL;
+}
