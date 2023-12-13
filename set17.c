370a3ccc41ba3958afd33cdaf872466e07aa6610
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-13 20:50:24 +0100

    Update set17.c

--- set17.c
+++ set17.c
@@ -68,7 +68,7 @@ void *my_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
     }
     min_size = old_size < new_size ? old_size : new_size;
     for (i = 0; i < min_size; i++) {
-        *(new_ptr + i) = *((char *)ptr + i);
+        new_ptr[i] = ((char *)ptr)[i];
     }
     free(ptr);
     return new_ptr;
