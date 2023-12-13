a4b0e38c2324ed7a4ea87ba5b3da94aae9839c0e
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-13 21:45:42 +0100

    Update set7.c

--- set7.c
+++ set7.c
@@ -27,28 +27,23 @@ char *_strncpy(char *str, const char *src, int n)
  * @n: it is amount of bytes to be maximally used
  * Return: returns the concatenated string
  */
-char *_strncat(char *str, const char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-    int str_len = 0;
-    int i;
-int max_append_len = n;
-    int src_len = 0;
-    while (str[str_len] != '\0')
-        str_len++;
-
-    
-    while (src[src_len] != '\0')
-        src_len++;
-    if (src_len < n)
-        max_append_len = src_len;
-
-    for (i = 0; i < max_append_len; i++)
-        str[str_len + i] = src[i];
-
-    /* Add the null-terminating character*/
-    str[str_len + i] = '\0';
-
-    return str;
+    int i, j;
+    char *s = dest;
+    i = 0;
+    j = 0;
+    while (dest[i] != '\0')
+        i++;
+    while (src[j] != '\0' && j < n)
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    if (j >= n)
+        dest[i] = '\0';
+    return s;
 }
 /**
  * _strchr - it  locates a character in a string
