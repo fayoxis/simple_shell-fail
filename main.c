221c7926965fbc56849221ea8f9dc2403752e3b3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-12-13 23:35:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main(int arg_c, char **arg_v)
     inform_t inform[] = {INFO_INIT};
     int fil_d = 2;
 
-    asm("mov %1, %0\n\t"
-        "add $3, %0"
+    asm ("mov %1, %0\n\t"
+		"add $3, %0"
         : "=r"(fil_d)
         : "r"(fil_d));
 
