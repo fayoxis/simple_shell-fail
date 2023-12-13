11a764900ce85b63c142d9d38286778a5d95141f
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-13 10:46:20 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+int main(int arg_c, char **arg_v)
+{
+    inform_t inform[] = {INFO_INIT};
+    int fil_d = 2;
+
+    asm("mov %1, %0\n\t"
+        "add $3, %0"
+        : "=r"(fil_d)
+        : "r"(fil_d));
+
+    if (arg_c == 2)
+    {
+        fil_d = open(arg_v[1], O_RDONLY);
+        if (fil_d == -1)
+        {
+            if (errno == EACCES)
+                exit(126);
+            if (errno == ENOENT)
+            {
+                _eputs(arg_v[0]);
+                _eputs(": 0: Can't open ");
+                _eputs(arg_v[1]);
+                _eputchar('\n');
+                _eputchar(BUF_FLUSH);
+                exit(127);
+            }
+            return EXIT_FAILURE;
+        }
+        inform->readfd = fil_d;
+    }
+
+    populateEnvironmentList(inform);
+    readHistoryFromFile(inform);
+    shellLoop(inform, arg_v);
+
+    return EXIT_SUCCESS;
+}
